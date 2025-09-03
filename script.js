9238192701a50855db4c6f529f5cdef8592b71bb
Motykiewicz <motykiewiczz@gmail.com>
2025-09-03 01:45:18 -0600

    Add info popup for game instructions
    Introduced a popup displaying game instructions, accessible via an 'Información' button in the header. Updated HTML structure, added related CSS for styling the popup, and modified JS to handle opening and closing the info.

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 //document.write("-");
 
+
+
+const overlay = document.getElementById("overlay");
+const popup = document.getElementById("popup");
+const closeBtn = document.getElementById("close-btn");
+
+
 document.getElementById("info-btn").onclick = function() {
-    document.getElementById("contenido-info").style.display = "flex";
+    document.getElementById("cuadro-info").style.display = "flex";
 };
 
 document.getElementById("cerrar-cuadro").onclick = function() {
-    document.getElementById("contenido-info").style.display = "none";
+    document.getElementById("cuadro-info").style.display = "none";
 }
