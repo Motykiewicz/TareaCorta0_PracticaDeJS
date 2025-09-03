65536024a0066801e0044d79392a211bbfdbb21f
Motykiewicz <motykiewiczz@gmail.com>
2025-09-02 18:21:40 -0600

    Add info functionality
    Introduces click handlers to show and hide the 'contenido-info' element when 'info-btn' and 'cerrar-cuadro' are clicked. Also adds a text file with non-APA formatted references.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //document.write("-");
+
+document.getElementById("info-btn").onclick = function() {
+    document.getElementById("contenido-info").style.display = "flex";
+};
+
+document.getElementById("cerrar-cuadro").onclick = function() {
+    document.getElementById("contenido-info").style.display = "none";
+}
